6a5126ec569b2e26576166e865abce38b875e2a4
Marin Barisic <barisic.m@outlook.com>
2020-09-06 02:01:39 +0200

    Webserver

--- fun-with-requests/server.go
+++ fun-with-requests/server.go
@@ -1,9 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-	// TODO: CREATE WEBSERVER
-
-	// TODO: LISTEN FOR REQUESTS
-
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":9090", nil))
 	// TODO: DUMP WEB REQUEST
 }
+
+func handler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w,"Hi there, i love %s!", r.URL.Path[1:])
+}
+
+
